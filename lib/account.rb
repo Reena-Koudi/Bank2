ffe6f7230ca8891e91e934a803be44029057133f
Reenakoudi <kreena92@gmail.com>
2018-04-24 11:18:53 +0100

    Added deposit method to Account and tested

--- lib/account.rb
+++ lib/account.rb
@@ -1,2 +1,12 @@
 class Account
+
+  DEFAULT_BALANCE = 0
+
+  def initialize(balance = DEFAULT_BALANCE)
+    @balance = balance
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,12 @@
 require 'account'
 
 describe Account do
+  subject(:account) { Account.new }
+
+  describe '#deposit' do
+    it 'adds ammount to the balance' do
+      expect(account.deposit(200)).to eq(200)
+    end
+  end
 
 end
