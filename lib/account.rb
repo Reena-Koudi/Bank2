649cea99d8b877415019919eb5d3a05138390075
Reenakoudi <kreena92@gmail.com>
2018-04-24 11:31:33 +0100

    Transactions recorded using the transactions array

--- lib/account.rb
+++ lib/account.rb
@@ -4,19 +4,22 @@ class Account
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+    @transactions = []
   end
 
   def deposit(amount)
     @balance += amount
+    @transactions.push(date: transaction_date, credit: amount, balance: @balance)
   end
 
   def withdrawal(amount)
     raise 'Balance is zero' if @balance == 0
     @balance -= amount
+    @transactions.push(date: transaction_date, debit: amount, balance: @balance)
   end
 
   private
-  def trasaction_date
+  def transaction_date
     Time.now.strftime('%d/%m/%Y')
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,14 +5,14 @@ describe Account do
 
   describe '#deposit' do
     it 'adds ammount to the balance' do
-      expect(account.deposit(200)).to eq(200)
+      expect(account.deposit(200)).to eq([{:date=>"24/04/2018", :credit=>200, :balance=>200}])
     end
   end
 
   describe '#withdrawal' do
     it 'deducts amount from the balance' do
       account.deposit(500)
-      expect(account.withdrawal(200)).to eq(300)
+      expect(account.withdrawal(200)).to eq([{:date=>"24/04/2018", :credit=>500, :balance=>500}, {:date=>"24/04/2018", :debit=>200, :balance=>300}])
     end
 
     it 'raises an error for the withdrawal if balance is zero' do
